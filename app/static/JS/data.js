cdde052971105ed763dc049b068208e60c28d61d
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2019-04-19 11:36:47 -0500

    fixed the issue with handlebars and added a new image to the menu page.

--- app/static/JS/data.js
+++ app/static/JS/data.js
@@ -1,7 +1,27 @@
 const BREAKFAST = {
-    foods: [{
-        name: "Cheese Omelet",
-        description: `Our Cheese Omelet is a delicious combinations of chesses Served with hashbrowns and toast. `,
-        price: "4.69"
-    }]
+    omelets: [{
+            name: "Cheese Omelet",
+            description: `Our Cheese Omelet is a delicious combinations of chesses Served with hashbrowns and toast. `,
+            price: 4.69
+        },
+
+        {
+            name: "Meat Omelet",
+            description: `Our Cheese Omelet is a delicious combinations of meat and chesses Served with hashbrowns and toast. `,
+            price: 5.69
+        },
+        {
+            name: "Western Omelet",
+            description: `Our Western Omelet is a delicious mixture of meat, onions, bell peppers, cheese, tomatoes, and mushrooms. Served with hashbrowns and toast.`,
+            price: 6.69
+        },
+    ],
+
+    plates: [
+        {
+            name: "Large Plate",
+            description: `Our Large Breakfast plate comes with 2 eggs, 2 pieces of bacon or 1 sausage, toast or biscuits, and hashbrowns or grits.`,
+            price: 5.29
+        }
+    ]
 }
--- app/static/JS/template.js
+++ app/static/JS/template.js
@@ -1,36 +1,41 @@
-// function settingUpFood() {
-//     var foods = document.querySelector("#omelets").innerHTML;
-//     var template = Handlebars.compile(foods);
-//     for (index in BREAKFAST.foods) {
-//         let food = BREAKFAST.foods[index];
-//         var html = template({
-//             index: index,
-//             food: food
-//         });
-//         document.querySelector("#omeletsTemplate").insertAdjacentHTML("beforeend", html);
-//     }
-// }
-
-// settingUpFood()
-
-
-function settingUpProjects() {
-    var projects = document.querySelector('#omelets').innerHTML;
-    console.log(projects)
-    var template = Handlebars.compile(projects);
-    console.log(BREAKFAST.foods)
-    for (index in BREAKFAST.foods) {
-        console.log(index)
-        let project = BREAKFAST.foods[index];
+function settingUpOmelets() {
+    var foods = document.querySelector("#omelets").innerHTML;
+    console.log(foods);
+    var template = Handlebars.compile(foods);
+    console.log(BREAKFAST.omelets);
+    for (index in BREAKFAST.omelets) {
+        console.log(index);
+        let omelets = BREAKFAST.omelets[index];
+        console.log(omelets.name)
         var html = template({
             index: index,
-            project: project
+            omelets: omelets
         });
         document
-            .querySelector('#omeletsTemplate')
-            .insertAdjacentHTML('beforeend', html);
+            .querySelector("#omeletsTemplate")
+            .insertAdjacentHTML("beforeend", html);
+        console.log(document.querySelector("#omeletsTemplate"));
     }
 }
 
+function settingUpPlates() {
+    var plate = document.querySelector("#plates").innerHTML;
+    console.log(plate);
+    var template = Handlebars.compile(plate);
+    console.log(BREAKFAST.plates);
+    for (index in BREAKFAST.plates) {
+        console.log(index);
+        let plates = BREAKFAST.plates[index];
+        console.log(plates.name)
+        var html = template({
+            index: index,
+            plates: plates
+        });
+        document
+            .querySelector("#platesTemplates")
+            .insertAdjacentHTML("beforeend", html);
+    }
+}
 
-settingUpProjects()
+settingUpOmelets();
+settingUpPlates()
