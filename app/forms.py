080393b729a8b089a6b0f45c546f06d3657ed569
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2019-04-24 16:25:23 -0500

    edited the form and reserved table page. I also used mailto to email the person on the reservation.

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class TableReservationForm(forms.Form):
     f_name = forms.CharField()
     l_name = forms.CharField()
-    p_number = forms.IntegerField()
+    p_number = forms.RegexField(regex=r'^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$')
     num_of_people = forms.IntegerField()
     date = forms.DateField()
     time = forms.TimeField()
--- app/models.py
+++ app/models.py
@@ -1,23 +1,27 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 # Create your models here.
 class TableReservation(models.Model):
     f_name = models.TextField()
     l_name = models.TextField()
-    p_number = models.IntegerField()
+    phone_regex = RegexValidator(
+        regex=r'^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$')
+    p_number = models.CharField(validators=[phone_regex], max_length=10)
     num_of_people = models.IntegerField()
     date = models.DateField()
     time = models.TimeField()
     email = models.EmailField()
 
     @staticmethod
-    def submit_reservation(f_name, l_name, p_number, num_of_people, date,
-                           time, email):
+    def submit_reservation(f_name, l_name, p_number, num_of_people, date, time,
+                           email):
         TableReservation(
             f_name=f_name,
             l_name=l_name,
             p_number=p_number,
             num_of_people=num_of_people,
             date=date,
-            time=time, email = email).save()
+            time=time,
+            email=email).save()
