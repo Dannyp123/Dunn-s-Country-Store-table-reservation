0b456e3283da182e1aca561205d6b0eccfd03187
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2019-05-01 13:02:51 -0500

    Working on pushing to heroku

--- dunns_store/settings.py
+++ dunns_store/settings.py
@@ -70,8 +70,8 @@ WSGI_APPLICATION = 'dunns_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'country-store',
     }
 }
 
@@ -114,6 +114,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'app/static'), )
+
 LOGIN_REDIRECT_URL = "home"
 LOGIN_URL = "login"
 
